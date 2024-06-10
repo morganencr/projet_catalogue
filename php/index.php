ba80a2874bddbcfecefece2b6561d2e68269da80
Guillaume-tllrd <guillaumetillard10@gmail.com>
2024-06-10 11:43:54 +0200

    ajout footer

--- php/index.php
+++ php/index.php
@@ -48,5 +48,7 @@ include_once("components/navbar.php");
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-
+<?php
+include_once("components/footer.php");
+?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
