93a68e2c96fab2641bb2e51198af62f49628423e
morganencr <morgane.encrenaz@gmail.com>
2024-06-10 09:49:58 +0200

    navbar et header

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -1,14 +1,36 @@
+<?php
+
+require_once("./../components/navbar.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>TOYS LAND</title>
 </head>
 <body>
-    <header>
+    <nav>
         <h1>TOYS LAND</h1>
-        
-    
+        <a><i class="fa-solid fa-user"></i></a>
+        <a><i class="fa-solid fa-cart-shopping"></i></a>
+    </nav>
+    <form action="" method="get">
+        <div>
+        <input type="search" placeholder="Recherchez un produit" id="searchbar">
+        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+        </div>
+    </form>
+    <ul class="categories">
+        <li><a>Jeux de société</a></li>
+        <li><a>Activités créatives</a></li>
+        <li><a>Jeux d'éveils</a></li>
+        <li><a>Jeux en bois</a></li>
+        <li><a>Jeux de construction</a></li>
+        <li><a>Jeux d'extérieurs</a></li>
+    </ul>
 </body>
 </html>
