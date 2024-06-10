98bd6e2b17b76091676864131a927de29b9a2641
morganencr <morgane.encrenaz@gmail.com>
2024-06-10 16:57:31 +0200

    navbar css

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -10,13 +10,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./navbar.css">
     <title>TOYS LAND</title>
 </head>
 <body>
     <nav>
         <h1>TOYS LAND</h1>
+        <div id="logos-navbar">
         <a><i class="fa-solid fa-user"></i></a>
         <a><i class="fa-solid fa-cart-shopping"></i></a>
+        </div>
     </nav>
     <form action="" method="get">
         <div>
