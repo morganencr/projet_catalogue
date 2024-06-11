b55221a4be7ffbe72f02da2d8390a9bf3b99d911
Guillaume-tllrd <guillaumetillard10@gmail.com>
2024-06-11 11:37:29 +0200

    fin du formulaire ajout

--- php/ajout/ajoutProduits.php
+++ php/ajout/ajoutProduits.php
@@ -1,79 +1,84 @@
 <?php
-if(!empty($_POST)){
+if(!empty($_POST))
+{
 // si $_POST N'est pas vide, on vérifie que toutes les données sont présentes
 
-    if(isset($_POST["nom"], $_POST["description"], $_POST["prix"], $_POST["categorie"], $_POST["stock"], $_POST["promo"], 
-    && !empty($_POST["nom"]) && !empty($_POST["descritption"])  && !empty($_POST["prix"]) && !empty($_POST["categorie"]) && !empty($_POST["stock"])&& !empty($_POST["promo"])){
+    if (isset($_POST["nom"], $_POST["description"], $_POST["prix"], $_POST["categorie"], $_POST["stock"], $_POST["promo"]) 
+    && !empty($_POST["nom"]) && !empty($_POST["description"]) && !empty($_POST["prix"]) && !empty($_POST["categorie"]) && !empty($_POST["stock"]) && !empty($_POST["promo"])) 
+    {
         // on retire toute balise du titre
         $nom = strip_tags($_POST["nom"]);
         // on neutralise toute balise du contenu (je les gardes mais elles ne sont plus active)
         $description = htmlspecialchars($_POST["description"]);
+        $prix = $_POST["prix"];
+        $categorie = $_POST["categorie"];
+        $stock = $_POST["stock"];
+        $promo = $_POST["promo"];
 
-        if(isset($_FILES["image"]) && $_FILES["image"]["error"] === 0)){
-        // on met ["image"] car c'est le type du fichier c'est ce qu'on trouve dans le var-dump
-        // on vérifie si le $_FILES image et image error est égal à 0 pour être sûr qu'on a une image
-        $imageTmpPath = $_FILES['image']['tmp_name'];
-        $imageName = $_FILES['image']['name'];
-        $imageSize = $_FILES['image']['size'];
-        $imageType = $_FILES['image']['type'];
-        // on récupère l'extension : 
-        // on a une fonction  qui s'appelle pathinfo() ou lui donne le nom du fichier 
-        $imageExtension = strtolower(pathinfo($imageName, PATHINFO_EXTENSION));
-        $allowed = [
-            "jpg" => "image/jpeg",
-            "jpeg" => "image/jpeg",
-            "png" => "image/png"
-        ];
-        if (!in_array($imageExtension, $allowed)) {
-            $error = "Type de fichier non autorisé. Seuls les fichiers JPG, JPEG, PNG et GIF sont autorisés.";
-        } else {
-            // $uploadDir = '../../uploads/';
-            //     $destinationPath = $uploadDir . $imageName;
-            //     if (move_uploaded_file($imageTmpPath, $destinationPath)) {
-            // à changer 
+        if (isset($_FILES["image"]) && $_FILES["image"]["error"] === UPLOAD_ERR_OK) 
+        {
+            // on met ["image"] car c'est le type du fichier c'est ce qu'on trouve dans le var-dump
+            // on vérifie si le $_FILES image et image error est égal à UPLOad_ERR_OK pour être sûr qu'on a une image
+
+            $imageTmpPath = $_FILES['image']['tmp_name'];
+            $imageName = $_FILES['image']['name'];
+            $imageSize = $_FILES['image']['size'];
+            $imageType = $_FILES['image']['type'];
+            // on récupère l'extension : 
+            // on a une fonction  qui s'appelle pathinfo() ou lui donne le nom du fichier 
+            $imageExtension = strtolower(pathinfo($imageName, PATHINFO_EXTENSION));
+            $allowed = [
+                "jpg" => "image/jpeg",
+                "jpeg" => "image/jpeg",
+                "png" => "image/png"
+            ];
+
+            if(!array_key_exists($imageExtension, $allowed) || !in_array($imageType, $allowed))
+            {
+            die("Type de fichier non autorisé. Seuls les fichiers JPG, JPEG, PNG et GIF sont autorisés.");
+            } else {
+            // on génère un nom unique : 
+            // uniqid est un TIMESTAMP et md5 permet de le chiffré histoire d'avoir un string aléatoire
+            $newname=md5(uniqid());
+            // on génère le chemin complet vers le fichier:
+            // on part d'une constante magique qui s'appelle __DIR__ qui correspond dans le dossier auquel je me trouve 
+            $newfilename = __DIR__ ."/uploads/$newname.$imageExtension";
+            if(!move_uploaded_file($_FILES["image"]["tmp_name"], $newfilename)){
+                die("l'upload a échoué");
+            }
+
                     // Se connecter à la base de données
+                    require_once("../connect.php");
+
+                    // Écrire la requête
+                    $sql = "INSERT INTO `produits`(`nom`, `description`, `image`, `prix`, `categorie`, `stock`, `promo`) VALUES (:nom, :description, :image, :prix, :categorie, :stock, :promo)";
+                    $query = $db->prepare($sql);
+
+                    // Lier les paramètres
+                    $query->bindValue(":nom", $nom, PDO::PARAM_STR);
+                    $query->bindValue(":description", $description, PDO::PARAM_STR);
+                    $query->bindValue(":image", $newfilename, PDO::PARAM_STR);
+                    $query->bindValue(":prix",  $_POST["prix"], PDO::PARAM_STR);
+                    $query->bindValue(":categorie", $_POST["categorie"], PDO::PARAM_STR);
+                    $query->bindValue(":stock", $_POST["stock"], PDO::PARAM_INT);
+                    $query->bindValue(":promo", $_POST["promo"], PDO::PARAM_STR);
+
+                        // Exécuter la requête et gérer les erreurs potentielles
+                        if ($query->execute()) {
+                            $id = $db->lastInsertId();
+                            header("Location: ../index.php");
+                            exit();
+                        } else {
+                            die("Une erreur est survenue lors de l'ajout de l'article.");
+                        }
+                    }
+                } else {
+                    die("Le téléchargement de l'image est obligatoire.");
+                }
+            } else {
+                die("Le formulaire est incomplet.");
+            }
         }
-         
-
-        
-        // ce sont les valeurs que l'on souhaite autorisé du fichier qui est envoyé, si c'est un pdf il nous le refusera
-
-        
-        
-
-        
-
-        //  on peut les enregistrer
-        // on se connecte à la base de données
-        require_once("../../connect.php");
-
-        // on écrit la requête
-        $sql = "INSERT INTO `produits`(`nom`, `description`, `image`, `prix`, `categorie`, `stock`, `promo`) VALUE (:nom, :description, :image, :prix, :categorie, :stock, :promo)";
-
-        // on prépare la requête
-        $query = $db->prepare($sql);
-
-        // on injecte les valeurs
-        $query->bindValue(":nom", $nom, PDO::PARAM_STR);
-        $query->bindValue(":description", $description, PDO::PARAM_STR);
-        $query->bindValue(":image", $image, PDO::PARAM_STR);
-        $query->bindValue(":prix",  $_POST["prix"], PDO::PARAM_STR);
-        $query->bindValue(":categorie", $_POST["categorie"], PDO::PARAM_STR);
-        $query->bindValue(":stock", $_POST["stock"], PDO::PARAM_INT);
-        $query->bindValue(":promo", $_POST["promo"], PDO::PARAM_STR);
-
-        // on execute la requeête 
-        if(!$query->execute()){
-            die("une erreur est survenue");
-        }
-
-        // on récupère l'id de l'article
-        $id = $db->lastInsertId();// ça nous permet de récupérer l'id du dernier insert executé
-        die("Article ajouté sous le numéro $id");
-    } else { 
-        die("Le formulaire est incomplet");
-    }
-}
 ?>
 
 <h1>AJOUTER UN PRODUIT</h1>
--- php/components/navbar.php
+++ php/components/navbar.php
@@ -10,11 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<<<<<<< HEAD
-    <!-- <link rel="stylesheet" href="output.css"> -->
-=======
-    <link rel="stylesheet" href="./navbar.css">
->>>>>>> 98bd6e2b17b76091676864131a927de29b9a2641
+    <link rel="stylesheet" href="../navbar.css">
     <title>TOYS LAND</title>
 </head>
 <body>
