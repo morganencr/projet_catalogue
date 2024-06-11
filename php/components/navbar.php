e089d8caeb05a3ec416f272e70ccfd4dc57d6699
morganencr <morgane.encrenaz@gmail.com>
2024-06-11 11:31:38 +0200

    css navbar

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -10,21 +10,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./navbar.css">
+    <link rel="stylesheet" href="../navbar.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Lilita+One&display=swap" rel="stylesheet">
     <title>TOYS LAND</title>
 </head>
 <body>
     <nav>
+        <div id="navbar-items">
+        <div id="logo-site">
+        <img src="./components/img/logo.site.png">
         <h1>TOYS LAND</h1>
-        <div id="logos-navbar">
-        <a><i class="fa-solid fa-user"></i></a>
-        <a><i class="fa-solid fa-cart-shopping"></i></a>
         </div>
-    </nav>
-    <form action="" method="get">
-        <div>
+        <div id="logos-navbar">
+        <a><i id="connexion" class="fa-solid fa-user fa-2x"></i></a>
+        <a><i id="panier" class="fa-solid fa-cart-shopping fa-2x"></i></a>
+        </div>
+        </div>
+        <form action="" method="get">
+        <div id="searchbar">
         <input type="search" placeholder="Recherchez un produit" id="searchbar">
-        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+        <button><i class="fa-solid fa-magnifying-glass fa-2x"></i></button>
         </div>
     </form>
     <ul class="categories">
@@ -35,5 +42,6 @@
         <li><a>Jeux de construction</a></li>
         <li><a>Jeux d'extérieurs</a></li>
     </ul>
+    </nav>
 </body>
 </html>
