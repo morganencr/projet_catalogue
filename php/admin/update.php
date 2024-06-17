84747db1af8468037f9160b14ed561e9970f4078
morganencr <morgane.encrenaz@gmail.com>
2024-06-17 15:26:25 +0200

    ajouts produits + bdd

--- php/admin/update.php
+++ php/admin/update.php
@@ -135,7 +135,7 @@ if(!empty($_POST))
             <option value="catégorie">Sélectionner une catégorie</option>
             <option value="Jeux de société">Jeux de société</option>
             <option value="Activité créative">Activités créatives</option>
-            <option value="Jeu d'éveil">Jeux d'éveils</option>
+            <option value="Jeux d'éveil">Jeux d'éveils</option>
             <option value="Jeu en bois">Jeux en bois</option>
             <option value="Jeu de construction">Jeux de construction</option>
             <option value="Jeux d'extérieur">Jeux d'extérieurs</option>
